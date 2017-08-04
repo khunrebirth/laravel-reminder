048c032855c12637915c5a6085e7accc53621f9f
ThanuSin <khunrebirth@gmail.com>
2017-08-04 14:51:29 +0700

    add title and edit write space

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,29 +2,31 @@
 @extends('template.main')
 
 {{-- Title --}}
-@section('title',  'Home Page')
+@section('title',  $title)
 
 {{-- Content --}}
 @section('content')
     @include('components.NavBar')
     <div class="container" style="margin: 80px; 0px;">
-        {{--  @if (Session::has('status'))
-            <div class="alert alert-success" role="alert">
-                {{ Session::get('status') }}
-            </div>
-        @endif  --}}
         <div class="row">
             <div class="col-md-12">
-                @include('components.ReminderList', [
-                    'reminders' => $reminders
-                ])
+                @include('components.ReminderList', ['reminders' => $reminders])
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                @include('components.NewReminder', [
-                    'types' => $types
-                ])
+                @include('components.NewReminder', ['types' => $types])
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-12">
+                @include('components.ReminderTypeList', ['types' => $types])
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                @include('components.NewReminderType')
             </div>
         </div>
     </div>
