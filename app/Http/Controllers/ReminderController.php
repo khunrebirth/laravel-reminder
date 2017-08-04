90680de67d102ad86118fac3f6cc09ec40533d37
ThanuSin <khunrebirth@gmail.com>
2017-08-04 11:57:01 +0700

    Add type in ReminderController.php

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\Reminder;
+use App\ReminderType;
 
 class ReminderController extends Controller
 {
     public function home()
     {
-        $data['reminders'] = Reminder::orderBy('id', 'desc')->get();
+        $data['reminders']  = Reminder::orderBy('id', 'desc')->get();
+        $data['types']      = ReminderType::get();
         return view('home', $data);
     }
 
@@ -20,6 +22,7 @@ class ReminderController extends Controller
         $reminder->body         = $request->reminder;
         $reminder->isFinished   = false;
         $reminder->createUserID = 1;
+        $reminder->ReminderType = $request->type;
         $reminder->save();
         return back();
     }
