6e986eb6bbf07d9404b800e88b4176d5e70b035c
ThanuSin <khunrebirth@gmail.com>
2017-08-04 15:35:40 +0700

    use SoftDeletes

--- app/Reminder.php
+++ app/Reminder.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reminder extends Model
 {
+    use SoftDeletes;
     protected   $table          = 'Reminder';
     public      $primaryKey     = 'id';
     public      $timestamps     = true;
