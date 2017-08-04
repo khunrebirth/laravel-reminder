8e23ae84aad5ad31b6869d0173e4a3021edb8245
ThanuSin <khunrebirth@gmail.com>
2017-08-04 11:57:34 +0700

    Add type to component

--- resources/views/components/ReminderList.blade.php
+++ resources/views/components/ReminderList.blade.php
@@ -1,12 +1,12 @@
 <h1>Your Reminder(s)</h1>
 @foreach($reminders as $reminder)
     <div class="well">
-        <span>{{ $reminder->body }}</span>
+        <span>{{ $reminder->body }} <b>({{ $reminder->getType->type }})</b></span>
         {{--  <a class="btn btn-success pull-right" href="/reminder/delete">Finish</a>  --}}
         <form method="post" action="/reminder/delete" style="display: inline;">
             {{ csrf_field() }}
             <input type="hidden" name="_method" value="delete">
-            <input type="hidden" name="reminderID" value="{{$reminder->id}}">
+            <input type="hidden" name="reminderID" value="{{ $reminder->id }}">
             <input type="submit" class="btn btn-success pull-right" value="FINISH">
         </form>
     </div>
