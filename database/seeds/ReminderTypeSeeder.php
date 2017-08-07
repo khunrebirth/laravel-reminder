2bbf1797f84d35126c89c2c384178b0269b02178
ThanuSin <khunrebirth@gmail.com>
2017-08-07 12:41:28 +0700

    put data

---
+++ database/seeds/ReminderTypeSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\ReminderType;
+
+class ReminderTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ReminderType::create([
+            'type' => 'Home',
+        ]);
+    }
+}
