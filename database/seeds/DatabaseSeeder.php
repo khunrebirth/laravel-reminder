b32225a764d3843fe4fecf64100b00af1a6fcb30
ThanuSin <khunrebirth@gmail.com>
2017-08-07 12:41:10 +0700

    add ReminderType, Reminder Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(ReminderTypeSeeder::class);
+        $this->call(ReminderSeeder::class);
     }
 }
