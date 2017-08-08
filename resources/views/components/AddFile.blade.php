bbf456a81cf9f9818c3c8364c7293da7eb9d2d93
ThanuSin <khunrebirth@gmail.com>
2017-08-08 20:20:55 +0700

    Add session UploadFileStatus

--- resources/views/components/AddFile.blade.php
+++ resources/views/components/AddFile.blade.php
@@ -1,3 +1,6 @@
+@if(session('UploadFileStatus'))
+    <div class="alert alert-success" role="alert">{{ session('UploadFileStatus') }}</div>
+@endif
 <form method="post" action="/file/upload" enctype="multipart/form-data">
     <h3>UPLOAD FILE</h3>
     <div class="form-group">
