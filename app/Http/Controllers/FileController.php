eb6c20d088ca8d8bf8a930f9af056c51e0427e8a
ThanuSin <khunrebirth@gmail.com>
2017-08-08 18:18:35 +0700

    Add method home()

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\File;
 
 class FileController extends Controller
 {
-    //
+    public function home() 
+    {   
+        $data['title']  = 'Files Page';
+        $data['files']  = File::all();
+        return view('files', $data);
+    }
 }
