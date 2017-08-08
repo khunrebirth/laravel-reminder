9823884eac2eab12ca208400b64d96a43ef0d9d3
ThanuSin <khunrebirth@gmail.com>
2017-08-08 17:40:56 +0700

    Add Migration Files Table

---
+++ database/migrations/2017_08_08_103602_create_files_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('FileName');
+            $table->text('FileExtension');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Files');
+    }
+}
