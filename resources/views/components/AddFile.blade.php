884731d7c6d1d86192dfabf4b8ba98676b29d735
ThanuSin <khunrebirth@gmail.com>
2017-08-08 18:29:13 +0700

    Chage input name

--- resources/views/components/AddFile.blade.php
+++ resources/views/components/AddFile.blade.php
@@ -1,12 +1,12 @@
-<form method="post" action="/file/upload">
+<form method="post" action="/file/upload" enctype="multipart/form-data">
     <h3>UPLOAD FILE</h3>
     <div class="form-group">
         <label>FILE NAME: </label>
-        <input type="text" name="name" class="form-control" placeholder="FILE NAME" required>
+        <input type="text" name="name" class="form-control" placeholder="FILE NAME" autofocus="" required>
     </div>
     <div class="form-group">
         <label>UPLOAD FILE: </label>
-        <input type="file" name="upload_file" class="form-control" placeholder="FILE NAME" required>
+        <input type="file" name="upload_file" class="form-control">
     </div>
     {{ csrf_field() }}
     <input type="submit" class="btn btn-primary" value="UPLOAD">
