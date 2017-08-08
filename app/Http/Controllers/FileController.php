70693432e6aab428c5b16ddbf21cd572ecaa1551
ThanuSin <khunrebirth@gmail.com>
2017-08-08 20:20:35 +0700

    Add method download() and put logic

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Storage;
-use App\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Input;
+
+use File;
+
+use App\Files;
 
 class FileController extends Controller
 {
     public function home() 
     {   
         $data['title']  = 'Files Page';
-        $data['files']  = File::all();
+        $data['files']  = Files::all();
 
         return view('files', $data);
     }
@@ -19,6 +23,23 @@ class FileController extends Controller
     public function upload(Request $request)
     {
         $fileName       = $request->name;
-        $uploadedFile   = $request->upload_file;
+        $extension      = Input::file('upload_file')->getClientOriginalExtension();
+        
+        Storage::disk('profiles')->put($fileName . '.' . $extension, File::get(Input::file('upload_file')));
+        
+        // Insert Data to Databse
+        $newFile                    = new Files();
+        $newFile->FileName          = $fileName;
+        $newFile->FileExtension     = $extension;
+
+        $newFile->save();
+
+        return back()->with(['UploadFileStatus' =>  'Success']);
+    }
+
+    public function download($filName)
+    {
+        // return response()->download('../storage/app/profiles', $filName);
+        return response()->download(storage_path("app/profiles/{$filName}"));
     }
 }
